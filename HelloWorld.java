4bfe6efa903855e235eb71f1f33f3c7dd1af4b16
ElhamYavari <e.yavari68@gmail.com>
2025-04-04 12:15:33 +0330

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        boolean b = false;
+        if (b) {
+            System.out.println(" b is boolean");
+        } else
+            System.out.println("b is false");
+    }
+}
