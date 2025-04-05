fbc9159a4c35af36b83735260ae63126d414c86b
ElhamYavari <e.yavari68@gmail.com>
2025-04-05 09:21:42 +0330

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,4 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        boolean b = false;
-        if (b) {
-            System.out.println(" b is boolean");
-        } else
-            System.out.println("b is false");
-    }
 }
